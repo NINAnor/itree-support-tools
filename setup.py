493d969a935d26011f761b4c358ade0476088bbe
willeke.acampo <willeke.acampo@nina.no>
2023-11-03 21:14:08 +0100

    config: change setup description

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ requirements = [
 setup(
     name="itree-support-tools",
     version="0.1.0",
-    description="This repo is a python template for windows.",
+    description="Tools for data preparation of iTree projects and extrapolation of iTree results.",
     license="MIT",
     author="willeke acampo",
     author_email="willeke.acampo@nina.no",
-    url="https://github.com/ac-willeke/urban-treeDetection",
+    url="https://github.com/ac-willeke/itree-support-tools",
     packages=find_packages(),
     install_requires=requirements,
 )
